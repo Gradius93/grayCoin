f5c521aeda9b1e2b842103e3ccf7216e4015962a
Sean Gray <sean.myles.gray@gmail.com>
2021-01-15 14:50:08 +0000

    dem changes"

---
+++ src/app/services/blockchain.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { Blockchain } from 'firstblockchain/src/blockchain'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BlockchainService {
+
+  constructor() { }
+}
